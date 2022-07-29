9715c3ad6e3cd2dc8f292740d74bda14a064d623
Jay B <46676014+jay-baldev@users.noreply.github.com>
2022-07-29 17:48:18 +0530

    Updated the str_cleanup and added bodmas without brackets
    -Updated the str_cleanup so it can take more than 1 character numbers 
    -added bodmas without support for brackets

--- calc.py
+++ calc.py
@@ -16,34 +16,57 @@ def str_cleanup(input_str):
     lst = []
     input_str = input_str.replace(" ", "")
     emp_str = ""
-
     for char in input_str:
-        if char == ".":
-            emp_str = lst[-1] + "."
-            del lst[-1]
-            continue
-        lst.append(emp_str + char)
-        emp_str = ""
+        if char == "/" or char == "*" or char == "+" or char == "-" :
+            lst.append(emp_str)
+            lst.append(char)
+            emp_str = ""
+        else:
+            emp_str = emp_str + char
+    lst.append(emp_str)
+    return lst
+
+def update_lst(i, lst):
+    num1 = float(lst[i-1])
+    op = lst[i]
+    num2 = float(lst[i+1])
+    
+    result = calc_main(num1, op, num2)
+    del lst[i-1:i+2]
+    lst.insert(i-1,result)
+    return lst
+
+def dmas(lst):
+    for i in range(len(lst)):
+        if lst[i] == "/":
+            lst = update_lst(i, lst)
+            # print(lst)
+            dmas(lst)
+            break
+    for i in range(len(lst)):
+        if lst[i] == "*":
+            lst = update_lst(i, lst)
+            dmas(lst)
+            break
+    for i in range(len(lst)):
+        if lst[i] == "+":
+            lst = update_lst(i, lst)
+            dmas(lst)
+            break
+    for i in range(len(lst)):
+        if lst[i] == "-":
+            lst = update_lst(i, lst)
+            dmas(lst)
+            break
     return lst
 
 
-
 def main():
-    # input_str = "1 + 2 * 3 / 4 - 5"
+    # input_str = "1.5 + 2 * 3 / 4 - 5"
     # input_str = "3.5 + 2"
     input_str = input()
     lst = str_cleanup(input_str)
-    
-    while len(lst) >= 3:
-        num1 = float(lst[0])
-        op = lst[1]
-        num2 = float(lst[2])
-        
-        result = calc_main(num1, op, num2)
-        del lst[0:3]
-        lst.insert(0,result)
-    
-    return print(result)
+    return print(dmas(lst)[0])
 
 
 if __name__ == "__main__":
